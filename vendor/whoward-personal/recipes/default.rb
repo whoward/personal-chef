57f4fca7e16c5d75c1f5d6e85a4dc2a52adf0de6
William Howard <whoward.tke@gmail.com>
2013-02-07 22:35:59 -0500

    update dependencies, add ssh keys from databags, dotfiles repository

--- vendor/whoward-personal/recipes/default.rb
+++ vendor/whoward-personal/recipes/default.rb
@@ -1,14 +1,12 @@
 
 #TODO: Use oracle java 7 instead of OpenJDK 6
-#TODO: install my personal dotfiles
-#TODO: perhaps my SSH settings? look into data bags perhaps?
 #TODO: jruby and 1.9.3 are not installing (no idea about rbx)
 
 Packages = %w(
    amarok ffmpeg google-chrome-stable google-musicmanager-beta gimp graphviz
    grive heroku-toolbelt htop imagemagick inkscape iotop memcached mongodb 
    mysql-server nginx openssh-server postgresql-9.1 postgresql-contrib-9.1 
-   skype steam sublime-text terminator virtualbox vlc wine1.5
+   skype steam sublime-text terminator virtualbox-4.2 vlc wine1.5
 )
 
 apt_repository "google-chrome" do
@@ -31,12 +29,19 @@ apt_repository "heroku-toolbelt" do
    key "https://toolbelt.heroku.com/apt/release.key"
 end
 
-apt_repository "steam" do
-   uri "http://repo.steampowered.com/steam/"
+apt_repository "valve-steam" do
+   uri "[arch=i386] http://repo.steampowered.com/steam/"
    distribution "precise"
    components ["steam"]
    keyserver "keyserver.ubuntu.com"
    key "B05498B7"
+end
+
+apt_repository "virtualbox" do
+   uri "http://download.virtualbox.org/virtualbox/debian"
+   distribution node['lsb']['codename']
+   components ["contrib"]
+   key "http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc"
 end
 
 apt_repository "ppa-webupd8team-sublime-text-2" do
@@ -89,14 +94,6 @@ apt_repository "ppa-nilarimogard-webupd8" do
    key "4C9D234C"
 end
 
-apt_repository "ppa-debfx-virtualbox" do
-   uri "http://ppa.launchpad.net/debfx/virtualbox/ubuntu"
-   distribution node['lsb']['codename']
-   components ["main"]
-   keyserver "keyserver.ubuntu.com"
-   key "62F1201A"
-end
-
 apt_repository "ppa-ubuntu-wine-ppa" do
    uri "http://ppa.launchpad.net/ubuntu-wine/ppa/ubuntu"
    distribution node['lsb']['codename']
@@ -116,4 +113,43 @@ end
 
 Packages.each do |package_name|
    package(package_name) { action :install }
+end
+
+# SSH Keys
+data_bag("keys").each do |key|
+   data = Chef::EncryptedDataBagItem.load("keys", key)
+
+   # private key
+   file "/home/will/.ssh/id_#{key}" do
+      owner   "will"
+      group   "will"
+      mode    "0600"
+      action  :create
+      content data["private"]
+   end
+
+   # public key
+   file "/home/will/.ssh/id_#{key}.pub" do
+      owner   "will"
+      group   "will"
+      mode    "0600"
+      action  :create
+      content data["public"]
+   end
+end
+
+# SSH Configuration File
+file "/home/will/.ssh/config" do
+   owner   "will"
+   group   "will"
+   mode    "0600"
+   action  :create
+   content Chef::EncryptedDataBagItem.load("config", "ssh-client")["file"]
+end
+
+# Dotfiles repository
+git "/home/will/.dotfiles" do
+   repository "git://github.com/whoward/dotfiles.git"
+   reference "master"
+   action :sync
 end
