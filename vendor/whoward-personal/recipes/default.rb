e9e7507e79bb53b905f09c57b9346845403a3dc2
William Howard <whoward.tke@gmail.com>
2013-02-09 17:53:02 -0500

    change ownership of dotfiles to me, link the rvm script if not linked

--- vendor/whoward-personal/recipes/default.rb
+++ vendor/whoward-personal/recipes/default.rb
@@ -149,14 +149,28 @@ git "/home/will/.dotfiles" do
    repository "git://github.com/whoward/dotfiles.git"
    reference "master"
    action :sync
+   user  "will"
+   group "will"
 end
 
+# symlink important files idempotently for .bashrc
 execute "bashrc-link-dotfiles" do
    command %Q{echo "source $HOME/.dotfiles/bashrc" >> /home/will/.bashrc}
    not_if  %Q{cat /home/will/.bashrc | grep "source $HOME/.dotfiles/bashrc"}
 end
 
+execute "bashrc-link-rvm" do
+   command %Q{echo "source $HOME/.rvm/scripts/rvm" >> /home/will/.bashrc}
+   not_if  %Q{cat /home/will/.bashrc | grep "source $HOME/.rvm/scripts/rvm"}
+end
+
+# symlink important files idempotently for .bashrc
 execute "bash_profile-link-dotfiles" do
    command %Q{echo "source $HOME/.dotfiles/bashrc" >> /home/will/.bash_profile}
    not_if  %Q{cat /home/will/.bash_profile | grep "source $HOME/.dotfiles/bashrc"}
+end
+
+execute "bash_profile-link-rvm" do
+   command %Q{echo "source $HOME/.rvm/scripts/rvm" >> /home/will/.bash_profile}
+   not_if  %Q{cat /home/will/.bash_profile | grep "source $HOME/.rvm/scripts/rvm"}
 end
