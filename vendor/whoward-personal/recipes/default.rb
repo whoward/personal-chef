9c7e9c752451043b76c9d2293ffd409c321c14c4
Will Howard <whoward.tke@gmail.com>
2015-05-05 22:04:06 -0400

    tie virtualbox to utopic for now - they haven't updated to vivid

--- vendor/whoward-personal/recipes/default.rb
+++ vendor/whoward-personal/recipes/default.rb
@@ -63,7 +63,7 @@ end
 
 apt_repository "virtualbox" do
    uri "http://download.virtualbox.org/virtualbox/debian"
-   distribution node['lsb']['codename']
+   distribution "utopic" # node['lsb']['codename']
    components ["contrib"]
    key "http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc"
 end
